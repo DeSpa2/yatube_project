93908d587a847efa110589b29a94c7952b2f752f
Денис Рассказов <DenSpas2@yandex.ru>
2022-10-06 22:37:19 +0800

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse(f'Страница группы {slug}')
+
+# Create your views here.
