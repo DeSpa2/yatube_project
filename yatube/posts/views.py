8204be74856dd62dd340a0e4c48cf9b7ff7513db
Денис Рассказов <DenSpas2@yandex.ru>
2022-10-19 20:46:59 +0800

    Я НЕ ПОНИМАЮ ЧТО ДЕЛАТЬ, НАСТАВНИК НЕ МОЖЕТ ПОМОЧЬ

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 
-# Страница со списком мороженого
+# Страница группы
 def group_posts(request, slug):
-    return HttpResponse(f'Страница группы {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
 
 # Create your views here.
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -55,8 +55,11 @@ ROOT_URLCONF = 'yatube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        # Добавлено: Искать шаблоны на уровне проекта
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+
+        # Оставляем True: шаблоны встроенных приложений (например, админки)
+        'APP_DIRS': True, 
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -119,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
